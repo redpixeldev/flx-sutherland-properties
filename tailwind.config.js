cb634e9efc523a1a26ad79a48f85eb7919cd8ed9
Daniela Guillen <daniela@redpixel.io>
2022-10-24 14:56:05 -0400

    Project setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,58 +1,29 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,njk,md}"],
   theme: {
     fontFamily: {
-      body: "",
-    },
+			baiJam: ['Bai Jamjuree', 'serif'],
+			lato: ['Lato', 'sans-serif'],
+		},
+		screens: {
+			xs: "530px",
+			sm: "640px",
+			md: "768px",
+			desk: "890px",
+			lg: "1024px",
+			xl: "1280px",
+			"2xl": "1536px",
+		},
 
-    container: {
-      center: true,
-      padding: "1rem",
-    },
-
-    extend: {
-      zIndex: {
-        "-1": "-1",
-      },
-      spacing: {
-        13: "3.25rem",
-        15: "3.75rem",
-        17: "4.25rem",
-        18: "4.5rem",
-        19: "4.75rem",
-        76: "19rem",
-        84: "21rem",
-        88: "22rem",
-        92: "23rem",
-        100: "25rem",
-        104: "26rem",
-        108: "27rem",
-        112: "28rem",
-        116: "29rem",
-        120: "30rem",
-        124: "31rem",
-        128: "32rem",
-        132: "33rem",
-        136: "34rem",
-        140: "35rem",
-        144: "36rem",
-        148: "37rem",
-        152: "38rem",
-        156: "39rem",
-        160: "40rem",
-        164: "41rem",
-        168: "42rem",
-        172: "43rem",
-        176: "44rem",
-        180: "45rem",
-        184: "46rem",
-        188: "47rem",
-        190: "48rem",
-        194: "49rem",
-        200: "50rem",
-      },
-    },
+		extend: {
+			colors: {
+				"tritonBlue": "#01529a",
+				"tritonGreen": "#27b061",
+				"tritonBlack": "#1f2022",
+				"tritonGray": "#818181",
+				"tritonLight": "#fbfbfb"
+			}
+		}
   },
   plugins: [
     require("@tailwindcss/typography"),
